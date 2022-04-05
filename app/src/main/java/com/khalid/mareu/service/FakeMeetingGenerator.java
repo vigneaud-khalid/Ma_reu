e339caf04924d84bf8cd1115f5745d2213e818e6
vigneaud-khalid <vigneaud40@yahoo.fr>
2022-04-05 19:54:33 +0200

    title & menufilter done

--- app/src/main/java/com/khalid/mareu/service/FakeMeetingGenerator.java
+++ app/src/main/java/com/khalid/mareu/service/FakeMeetingGenerator.java
@@ -2,6 +2,9 @@ package com.khalid.mareu.service;
 
 import com.khalid.mareu.model.Meeting;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
@@ -13,14 +16,22 @@ import java.util.List;
  */
 public abstract class FakeMeetingGenerator {
 
+//    SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+//    Date date = formatter.parse("2012-03-04");
+
+
     public static List<Meeting> MEETINGS = Arrays.asList(
 
+
             new Meeting(1, "Réunion A", "Peach", null, null ),
-            new Meeting(2, "Réunion B", "Room 2", new Date(), Collections.singletonList("Delhi"))
-            // ,
+            //new Meeting(2, "Réunion B", "Room 2", new Date(), Collections.singletonList("Delhi"))
+            //
             // new Meeting(4,"Réunion C","Room 3", 2012-03-04, null),
-            // new Meeting(4,"Réunion D","Room 4", 2012-03-04, {"maxime@lamzone.com", "alex@lamzone.com"})
-            );
+            new Meeting(4,"Réunion D","Room 4", new Date(), Arrays.asList("maxime@lamzone.com", "alex@lamzone.com"))
+            // new Meeting(5,"Réunion E","Room 5", date, Arrays.asList("Hello", "World!", "How", "Are", "You"))
+        );
+
+    // "dd-MM-yyyy"
     static List<Meeting> generateMeetings() {
         return new ArrayList<>(MEETINGS);
     }
--- app/src/main/java/com/khalid/mareu/ui/ListMeetingActivity.java
+++ app/src/main/java/com/khalid/mareu/ui/ListMeetingActivity.java
@@ -7,27 +7,28 @@ import androidx.fragment.app.FragmentActivity;
 import androidx.viewpager.widget.ViewPager;
 
 import com.khalid.mareu.R;
+import com.khalid.mareu.databinding.ActivityListMeetingBinding;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
 
-import butterknife.BindView;
-import butterknife.ButterKnife;
 import butterknife.OnClick;
 
 public class ListMeetingActivity extends AppCompatActivity {
 
-    @BindView(R.id.menufilter)
-    Toolbar mToolbar;
-    @BindView(R.id.container)
-    ViewPager mViewPager;
+
+    private ActivityListMeetingBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_meeting);
-        ButterKnife.bind(this);
-        setSupportActionBar(mToolbar);
+        binding = ActivityListMeetingBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+        setSupportActionBar(binding.appbar);
     }
 
     @OnClick(R.id.add_meeting)
@@ -35,8 +36,9 @@ public class ListMeetingActivity extends AppCompatActivity {
         AddMeetingActivity.navigate(this);
     }
 
-    @OnClick(R.id.menufilter)
-    void filter() {
-        // todo
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_filter, menu);
+        return  true;
     }
 }
